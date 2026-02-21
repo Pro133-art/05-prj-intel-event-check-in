300daaf087f36ccb12b0e4098bfd200399c9adca
Winston Li <liwinsto@msu.edu>
2026-02-21 03:54:41 +0000

    Add DOM element references for check-in form

---
+++ script.js
@@ -0,0 +1,4 @@
+// Get Some DOM Elements
+const form = document.getElementById("checkInForm");
+const nameInput = document.getElementById("attendeeName");
+const teamSelect = document.getElementById("teamSelect");
