e8469e572ba51e04fa50f54ef13e4ca28b1ab5e1
Winston Li <liwinsto@msu.edu>
2026-02-21 04:11:22 +0000

    Add attendance tracking to check-in form submission

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const form = document.getElementById("checkInForm");
 const nameInput = document.getElementById("attendeeName");
 const teamSelect = document.getElementById("teamSelect");
 
+// Track attendence
+ let count = 0;
+ const maxCount = 50;
+
+
+
 // Handle Submission Button
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-  // Get values from the form
+
+  // Get form values
   const name  = nameInput.value;
   const team = teamSelect.value;
   const teamName = teamSelect.selectedOptions[0].text;
@@ -14,4 +21,8 @@ form.addEventListener("submit", function(event) {
 
 
   console.log(name, team, teamName);
+
+  // Increment count 
+  count++;
+  console.log("Total check-ins: ", count);
 });
