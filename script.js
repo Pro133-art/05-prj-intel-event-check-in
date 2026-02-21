199d3cdea14cba80c6d96dab27f6d10465344c29
Winston Li <liwinsto@msu.edu>
2026-02-21 04:06:45 +0000

    Implement form submission handling for check-in process

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 const form = document.getElementById("checkInForm");
 const nameInput = document.getElementById("attendeeName");
 const teamSelect = document.getElementById("teamSelect");
+
+// Handle Submission Button
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  // Get values from the form
+  const name  = nameInput.value;
+  const team = teamSelect.value;
+  const teamName = teamSelect.selectedOptions[0].text;
+
+
+
+  console.log(name, team, teamName);
+});
